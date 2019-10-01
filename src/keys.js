69c22fdeca9bdfd5ca59efb79b7bd87fd1e6636d
Jair Cruzado <lawcodepe@gmail.com>
2019-10-01 17:59:29 -0500

    ok

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-    database:{
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'database_links'
-    }
+  database:{
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'database_links'
+  }
 }
